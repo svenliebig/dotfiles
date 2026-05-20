d13008951efb5e44fbbe78614e4f9b860ced74cd
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2026-05-21 00:20:06 +0200

    feat: improve line numbers switch performance

--- config/nvim/lua/custom/plugins/linenumbers.lua
+++ config/nvim/lua/custom/plugins/linenumbers.lua
@@ -1,5 +1,27 @@
-vim.cmd [[
-  source ~/.config/nvim/lua/custom/plugins/linenumbers.vim
-]]
+-- Show absolute line numbers; switch to relativenumber in normal mode of the
+-- focused window, and back to absolute when leaving the buffer/window or
+-- entering insert mode.
+vim.o.number = true
+vim.o.relativenumber = true
+
+local group = vim.api.nvim_create_augroup('numbertoggle', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave' }, {
+  group = group,
+  callback = function()
+    if vim.wo.number then
+      vim.wo.relativenumber = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter' }, {
+  group = group,
+  callback = function()
+    if vim.wo.number then
+      vim.wo.relativenumber = false
+    end
+  end,
+})
 
 return {}
--- config/nvim/lua/custom/plugins/linenumbers.vim
+++
@@ -1,7 +0,0 @@
-:set number
-:augroup numbertoggle
-:  autocmd!
-:  autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
-:  autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
-:augroup END
-
