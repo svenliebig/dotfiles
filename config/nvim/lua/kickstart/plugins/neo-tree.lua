0796ab62a328850b5c1f0b1360a98ef167a577d5
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2026-05-21 00:20:23 +0200

    fix: improve neotree startup performance

--- config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -9,10 +9,23 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  lazy = false,
+  cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
+  init = function()
+    -- Disable netrw entirely; neo-tree takes over directory views.
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    -- If nvim was opened on a directory, eagerly load neo-tree so it
+    -- shows up instead of an empty buffer.
+    if vim.fn.argc(-1) == 1 then
+      local stat = vim.uv.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == 'directory' then
+        require('neo-tree')
+      end
+    end
+  end,
   opts = {
     filesystem = {
       filtered_items = {
